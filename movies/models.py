3805967b4771ac98891a17336d7c2886c29d524a
woo <woodg1207@gmail.com>
2019-11-20 15:21:08 +0900

    first commit

---
+++ movies/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+    
+class Movie(models.Model):
+    title = models.CharField(max_length=30)
+    audience = models.IntegerField()
+    poster_url = models.CharField(max_length=140)
+    description = models.TextField()
+    genres = models.ManyToManyField(Genre, related_name='movies', blank=True)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, 
+        related_name='like_movies',
+        blank=True)
+
+class Review(models.Model):
+    content = models.CharField(max_length=140)
+    score = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
