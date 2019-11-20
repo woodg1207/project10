36449fe64bf13958a632baa75ec26a0c83ad95db
woo <woodg1207@gmail.com>
2019-11-20 15:43:29 +0900

    second commit

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Genre, Movie, Review
 # Register your models here.
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('content',)
